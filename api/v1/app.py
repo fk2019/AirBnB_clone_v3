e799f135edbc244a11b7bb2c4a15f518756ee486
fk2019 <fk2019@gmail.com>
2023-05-21 15:14:40 +0300

    Add app.py

--- api/v1/app.py
+++ api/v1/app.py
@@ -2,19 +2,28 @@
 """api module that registeres app_views blueprint
    and handle teardown_appcontext
 """
-from flask import Flask
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify, make_response
+from flask_cors import CORS
+from models import storage
 from os import getenv
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
+cors = CORS(app, resources={'/*': {'origin': '0.0.0.0'}})
 
 
 @app.teardown_appcontext
 def teardown_db(exception):
     """closes the storage on teardown"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """Return jasonified error"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
