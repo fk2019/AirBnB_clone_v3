d8b5fa83f9263568e21a85b476e4fa7515fe24b9
fk2019 <fk2019@gmail.com>
2023-05-21 15:48:48 +0300

    Addstats

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,9 +3,24 @@
 """
 from api.v1.views import app_views
 from flask import jsonify
+from models import storage
+
+hbnb = {'amenities': 'Amenity', 'cities': 'City',
+        'places': 'Place', 'reviews': 'Review',
+        'states': 'State', 'users': 'User'
+        }
 
 
 @app_views.route('/status', strict_slashes=False)
 def status():
     """Return JSON OK status"""
     return jsonify({'status': 'OK'})
+
+
+@app_views.route('/stats', strict_slashes=False)
+def stats():
+    """Return number of each object"""
+    result = {}
+    for key, value in hbnb.items():
+        result[key] = storage.count(value)
+    return jsonify(result)
