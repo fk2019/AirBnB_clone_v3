eba6f5445421202d54bde68b1aab504ecdcb883f
fk2019 <fk2019@gmail.com>
2023-05-24 21:24:03 +0300

    Add small changes

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,13 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const amenity_ids = []
-    $(#check_amenity).on('change', () => {
-	const amenity_id = $(this).data('id')
-	if ($(this).is(':checked')) {
+  const amenity_ids = []
+  $('#check_amenity input[type=checkbox]').on('click', (event) => {
+	  let amenity_id = ''
+    console.log($("input:checked").val())
+	  /*if ($(this).is(':checked')) {
+      console.log('clicked')
 	    amenity_ids.push(amenity_id)
-	} else {
+	  } else {
+      console.log('Nooo')
 	    const index = amenity_ids.indexOf(amenity_id)
 	    if (index !== -1) {
-		amenity_ids.splice(index, 1)
-	}
-    })
-}
+		    amenity_ids.splice(index, 1)
+	    }
+    }*/
+  })
+})
